a5b3cb5786a1666fa34657f4a6253177e294617d
Santosh Narayanan <santosh33@gmail.com>
2025-08-04 20:34:53 +0200

    Sample testing of CORS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class connectionExample extends React.Component {
+  componentDidMount() {
+    const apiUrl = 'http://127.0.0.1:8000/api/';
+    fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Data fetched successfully:', data);
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Connection Example</h1>
+        <p>This is a simple example of a React component.</p>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default connectionExample;
